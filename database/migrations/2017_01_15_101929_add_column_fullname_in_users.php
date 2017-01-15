3e4eeedafca3e6e8d807e6f4cca552be56a94872
januita <januitamarpaung@gmail.com>
2017-01-15 17:22:35 +0700

    update 15-1-2017 5:22

---
+++ database/migrations/2017_01_15_101929_add_column_fullname_in_users.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnFullnameInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE `users` ADD `fullname` VARCHAR(255) NOT NULL AFTER `username`")
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
